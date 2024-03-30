2c87781219cc7ce6a09ba77b43ddd567d5a3adba
Bhanu Nandana Pavan Kumar Bavirisetty <bnpavankumar.bavirisetty1998@gmail.com>
2024-03-29 21:20:52 -0500

    nestedrouting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom'
+import Home from './Home'
+import About  from './About'
 
-function App() {
+function Header()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+    </>
+  );
+}
+function App()
+{
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      </Routes>
+    
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+function Contact()
+{
+    
+    return (
+        <>
+        <h1>This is a Contact Component</h1>
+        
+        </>
+    );
+}
+
+export default Contact
